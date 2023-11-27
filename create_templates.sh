43f9e75415071a6fbf2231a265473b004b09ded2
realcryptonight <me@realcryptonight.dev>
2023-11-27 12:44:02 +0100

    Added: DirectAdmin to the create template script

--- create_templates.sh
+++ create_templates.sh
@@ -10,5 +10,19 @@ qm set 900 --boot order=scsi0 --scsihw virtio-scsi-single
 qm set 900 --agent enabled=1,fstrim_cloned_disks=1
 qm set 900 --ide2 local:cloudinit
 qm set 900 --cicustom "user=local:snippets/standard.yaml"
-qm disk resize 900 scsi0 32G
+qm disk resize 900 scsi0 200G
 qm template 900
+
+qm create 901 --name directadmin-template --ostype l26
+qm set 901 --net0 virtio,bridge=vmbr0
+qm set 901 --serial0 socket --vga serial0
+qm set 901 --memory 16384 --cores 4 --cpu host
+qm set 901 --scsi0 local:0,import-from="$(pwd)/debian-12-generic-amd64.qcow2",discard=on,ssd=1
+qm set 901 --boot order=scsi0 --scsihw virtio-scsi-single
+qm set 901 --agent enabled=1,fstrim_cloned_disks=1
+qm set 901 --ide2 local:cloudinit
+qm set 901 --cicustom "user=local:snippets/directadmin.yaml"
+qm disk resize 901 scsi0 200G
+qm template 901
+
+rm debian-12-generic-amd64.qcow2
