757882687fd799115ac011905501b44a8ff3ef24
realcryptonight <me@realcryptonight.dev>
2023-11-27 14:24:33 +0100

    Added: auto vm boot on host boot

--- create_templates.sh
+++ create_templates.sh
@@ -23,6 +23,7 @@ if ! grep -q "vmid: 900" "vmidcheck.txt" ; then
 	qm set 900 --memory 16384 --cores 4 --cpu host
 	qm set 900 --scsi0 local:0,import-from="$(pwd)/debian-12-generic-amd64.qcow2",discard=on,ssd=1
 	qm set 900 --boot order=scsi0 --scsihw virtio-scsi-single
+	qm set 900 --onboot 1
 	qm set 900 --agent enabled=1,fstrim_cloned_disks=1
 	qm set 900 --ide2 local:cloudinit
 	qm set 900 --cicustom "user=local:snippets/standard.yaml"
@@ -37,6 +38,7 @@ if ! grep -q "vmid: 901" "vmidcheck.txt" ; then
 	qm set 901 --memory 16384 --cores 4 --cpu host
 	qm set 901 --scsi0 local:0,import-from="$(pwd)/debian-12-generic-amd64.qcow2",discard=on,ssd=1
 	qm set 901 --boot order=scsi0 --scsihw virtio-scsi-single
+	qm set 900 --onboot 1
 	qm set 901 --agent enabled=1,fstrim_cloned_disks=1
 	qm set 901 --ide2 local:cloudinit
 	qm set 901 --cicustom "user=local:snippets/directadmin.yaml"
@@ -45,3 +47,4 @@ if ! grep -q "vmid: 901" "vmidcheck.txt" ; then
 fi
 
 rm debian-12-generic-amd64.qcow2
+rm vmidcheck.txt
