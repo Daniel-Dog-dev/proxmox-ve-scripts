c8304ace5565a8fa99192695a31a875e76452fc6
Daniel-Dog-dev <y33w4ayg@daniel-dog.com>
2024-12-28 23:16:43 +0100

    Fixed: user creation for sftp script

--- files/SFTP/add_ssh_user.sh
+++ files/SFTP/add_ssh_user.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if a username is given.
 if [ -z "$1" ]
 	then
 		echo "./add_ssh_user.sh <ssh username>"
@@ -9,20 +8,10 @@ if [ -z "$1" ]
 		echo "Username given. Continue."
 fi
 
-# Create a user with the home directory in the /backups folder.
-adduser --disabled-password --group sftpbackup --home /sftp-backups/$1 $1
-
-# Make the user and sftpgroup owner of the home directory.
-chown $1:sftpbackup /sftp-backups/$1/
-
-# Make the home directory only accessible by the user.
-chmod 700 /sftp-backups/$1/
-
-# Add the .ssh directory to the user home directory.
+adduser --disabled-password --gecos "" --home /sftp-backups/$1 $1
 mkdir /sftp-backups/$1/.ssh
 
-# Make the user the only owner of the .ssh directory.
+chown $1:sftpbackup /sftp-backups/$1/
 chown $1:$1 /sftp-backups/$1/.ssh/
+chmod -R 700 /sftp-backups/$1/
 
-# Make the .ssh directory only accessible by the user.
-chmod 700 /sftp-backups/$1/.ssh/
