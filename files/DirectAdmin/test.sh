42b4d78fcf0bee8370e06cc97e9cc63b28892713
Daniel-Dog-dev <y33w4ayg@daniel-dog.com>
2025-08-27 11:38:06 +0200

    Added: debug test script

---
+++ files/DirectAdmin/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the hostname and domain name for NS records.
+serverip=""
+retries=0
+while [[ $(hostname -I | awk '{print $1}') =~ "" ]] && [ ! $retries -gt 6 ];; do
+		let "retries++"
+		sleep 20s
+done
+
+serverip=$(hostname -I | awk '{print $1}')
+serverhostname=$(dig -x $serverip +short | sed 's/\.[^.]*$//')
+domainhostname=$(echo $serverhostname | sed 's/^[^.]*.//g')
+ns1host="ns1.${domainhostname}"
+ns2host="ns2.${domainhostname}"
+
+echo "IP: ${serverip}" >> test.txt
+echo "domain: ${domainhostname}" >> test.txt
